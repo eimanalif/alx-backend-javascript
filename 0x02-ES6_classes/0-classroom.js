9b3a6a4af1694e4977cd61625e66eacf97698618
eimanalif <eiman.fadlelmowla@gmail.com>
2024-06-27 09:11:33 +0000

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  /**
+   * a classroom taking a number as an output
+   * as maximum number of student
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
