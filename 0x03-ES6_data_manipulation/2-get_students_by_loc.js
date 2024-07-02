48b009119c226e20fb1ad2366aaeaf285339ea06
eimanalif <eiman.fadlelmowla@gmail.com>
2024-07-02 05:14:02 +0000

    task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,10 +1,8 @@
-import getListStudents from './0-get_list_students.js'
-
 /**
  * a function that returns an array of objects
  * who are located in a specific city.
  */
 
 export default function getStudentsByLocation(students, city) {
-  return students.filter( students => students.location === city);
+  return students.filter((students) => students.location === city);
 }
