c7163e465dc4ec659d25e844e84f004e18d213cf
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-22 19:26:13 +0000

    task5

--- 0x06-unittests_in_js/ 5-payment.test.js
+++ 0x06-unittests_in_js/ 5-payment.test.js
@@ -1,26 +1,37 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToAPI = require('./5-payment'); // Adjust this path based on your project structure
 
-describe('sendPaymentRequestToApi', () => {
-  let consoleSpy;
+describe('sendPaymentRequestToAPI', function () {
+    let consoleSpy;
 
-  beforeEach(() => {
-    consoleSpy = sinon.spy(console, 'log');
-  });
+    beforeEach(function () {
+        // Create a spy on console.log
+        consoleSpy = sinon.spy(console, 'log');
+    });
 
-  afterEach(() => {
-    consoleSpy.restore();
-  });
+    afterEach(function () {
+        // Restore the original console.log
+        consoleSpy.restore();
+    });
 
-  it('should log the correct total for 100 and 20', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
-  });
+    it('should log "The total is: 120" and only be called once when called with 100 and 20', function () {
+        sendPaymentRequestToAPI(100, 20);
 
-  it('should log the correct total for 10 and 10', () => {
-    sendPaymentRequestToApi(10, 10);
-    expect(consoleSpy.calledOnceWith('The total is: 20')).to.be.true;
-  });
+        // Check that console.log was called with the correct string
+        expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+
+        // Check that console.log was called only once
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
+
+    it('should log "The total is: 20" and only be called once when called with 10 and 10', function () {
+        sendPaymentRequestToAPI(10, 10);
+
+        // Check that console.log was called with the correct string
+        expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+
+        // Check that console.log was called only once
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
 });
