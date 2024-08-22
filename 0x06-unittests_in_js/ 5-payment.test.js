bb3828f7658228c8f9055fdc3a7c3f8da12f551c
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-08-22 18:59:51 +0000

    task5

--- 0x06-unittests_in_js/ 5-payment.test.js
+++ 0x06-unittests_in_js/ 5-payment.test.js
@@ -1,26 +1,29 @@
-const { expect } = require('chai');
 const sinon = require('sinon');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  let consoleSpy;
+  let bigBrother;
 
   beforeEach(() => {
-    consoleSpy = sinon.spy(console, 'log');
+    if (!bigBrother) {
+      bigBrother = sinon.spy(console);
+    }
   });
 
   afterEach(() => {
-    consoleSpy.restore();
+    bigBrother.log.resetHistory();
   });
 
-  it('should log the correct total for 100 and 20', () => {
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
+    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
   });
 
-  it('should log the correct total for 10 and 10', () => {
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(consoleSpy.calledOnceWith('The total is: 20')).to.be.true;
+    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
   });
 });
--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -5,3 +5,15 @@ const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
   });
   
   module.exports = getPaymentTokenFromAPI;
+  const { expect } = require('chai');
+  const getPaymentTokenFromAPI = require('./6-payment_token');
+  
+  describe('getPaymentTokenFromAPI', () => {
+    it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+      getPaymentTokenFromAPI(true)
+        .then((res) => {
+          expect(res).to.deep.equal({data: 'Successful response from the API'});
+          done();
+        });
+    });
+  });
